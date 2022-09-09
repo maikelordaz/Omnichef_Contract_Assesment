d84dc5c1e011a266c9561e215f87e709635aa577
maikelordaz <maikelordaz@gmail.com>
2022-09-08 22:57:04 -0400

    Coments added
    NatSpec and coments added

--- contracts/libs/SafeArithmetics.sol
+++ contracts/libs/SafeArithmetics.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 
+/**
+ * @title SafeArithmetics
+ * @notice A library with safe arithmetics operations to avoid overflows and underflows
+ */
+
 pragma solidity ^0.8.0;
 
 library SafeArithmetics {
+    /**
+     * @notice The arithmetics operations to perform
+     */
     enum Operation {
         ADD,
         SUB,
@@ -11,10 +19,32 @@ library SafeArithmetics {
         POW
     }
 
+    /**
+     * @notice A function to perform Operations when two parameters are received
+     * @param a Value to operate
+     * @param op operation to execute
+     * @return the result of the operation
+     * @dev It calls another function with the same name, but this last one receive three
+     * parameters
+     */
     function safe(uint256 a, Operation op) internal pure returns (uint256) {
         return safe(a, op, a);
     }
 
+    /**
+     * @notice A function to perform Operations when three parameters are received
+     * @param a Value to operate
+     * @param b Value to operate
+     * @param op operation to execute
+     * @return it returns a
+     * @dev It is called by another function with the same name, but this last one receive two
+     * parameters
+     * @dev For ADDING "a" must be the greater
+     * @dev For SUBSTRACTION "b" must be the greater
+     * @dev For DIVIDING "b" must be different than zero
+     * @dev The operations SUBSTRACTION is wrong
+     * @dev It always return a after the operations
+     */
     function safe(
         uint256 a,
         Operation op,
@@ -35,7 +65,7 @@ library SafeArithmetics {
             a /= b;
         } else if (op == Operation.POW) {
             uint256 c = a;
-            a ** b;
+            a**b;
             require(a >= c || a == 1);
         }
 
