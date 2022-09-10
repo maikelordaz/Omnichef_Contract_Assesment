3ab2197b334c4881adc8bd3d0b919a72d322b919
maikelordaz <maikelordaz@gmail.com>
2022-09-09 22:14:59 -0400

    Final suggested code

--- contracts/strategies/OmniCompoundStrategy.sol
+++ contracts/strategies/OmniCompoundStrategy.sol
@@ -27,62 +27,38 @@ interface ICEth {
 contract OmniCompoundStrategy {
     using SafeArithmetics for uint256;
 
-    /// @dev Finding [H02]
     ICEth private CEth;
 
-    /**
-     * It sets the ICEth interface according to the call of OmniChef
-     */
     constructor(address _CEth) {
         CEth = ICEth(_CEth);
     }
 
-    // Deposit funds into the Compound ERC20 token
-    // It is public so anybody can call it and deposit all the contract balance at any time
-    function deposit() public {
+    function deposit() internal {
         _send(payable(address(CEth)), address(this).balance);
     }
 
     // Compound funds acquired from interest on Compound
     function compound() external {
-        // Calculates the interes
         CEth.accrueInterest();
-        // Call the unlock function on this same contract
-        OmniCompoundStrategy(address(this)).unlock();
-        // calls deposit
         deposit();
-    }
-
-    // Allow invocation only by self for compounding
-    function unlock() external {
-        // verify the caller
-        require(msg.sender == address(this), "INSUFFICIENT_PRIVILEGES");
-        // It calls the function _unlock on this contract
-        _unlock(balance());
     }
 
     // Calculate total balance
     function balance() public view returns (uint256) {
-        // contract´s balance + contracts CEth balance of underlying
         return address(this).balance + CEth.balanceOfUnderlying(address(this));
     }
 
-    // Public or internal????
-    function _unlock(uint256 amount) public {
-        // If the amount is bigger than the contract´s balance redeem the needed
+    function _unlock(uint256 amount) internal {
         if (amount > address(this).balance)
             CEth.redeem(
-                // (amount - contract´s balance) * contract´s CEth balance / contract´s CEth balance of underlying
                 (amount - address(this).balance)
                     .safe(SafeArithmetics.Operation.MUL, CEth.balanceOf(address(this)))
                     .safe(SafeArithmetics.Operation.DIV, CEth.balanceOfUnderlying(address(this)))
             );
 
-        // transfer the caller the amount
         _send(payable(msg.sender), amount);
     }
 
-    /// @dev Finding [H01]
     function _send(address payable target, uint256 amount) internal {
         target.transfer(amount);
     }
